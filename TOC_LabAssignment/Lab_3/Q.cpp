3adaee7ee35b0f435b6e57294d21f7f5b7610093
Rohit-110 <120464365+Rohit-110@users.noreply.github.com>
2023-10-09 09:52:10 +0530

    Add files via upload

--- TOC_LabAssignment/Lab_3/Q.cpp
+++ TOC_LabAssignment/Lab_3/Q.cpp
@@ -46,3 +46,49 @@ int main(){
     if(ans)cout<<"String is accepted"<<endl;
     else cout<<"Rejected"<<endl;
 }
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Second way Using sets
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<vector<vector<int>>>tran{
+        {{-1},{1},{2}},{{1,2},{2},{-1}},{{0},{-1},{-1}}
+    };
+    queue<int>st;
+    st.push(0);
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        set<int>a;
+        while(!st.empty()){
+            for(int j=0;j<tran[st.front()][s[i]-'a'].size();j++){
+                if(tran[st.front()][s[i]-'a'][j]!=-1)
+                a.insert(tran[st.front()][s[i]-'a'][j]);
+            }
+            for(int j=0;j<tran[st.front()][2].size();j++){
+                if(tran[st.front()][2][j]!=-1){
+                    for(int k=0;k<tran[tran[st.front()][2][j]][s[i]-'a'].size();k++){
+                        if(tran[tran[st.front()][2][j]][s[i]-'a'][k]!=-1)
+                        a.insert(tran[tran[st.front()][2][j]][s[i]-'a'][k]);
+                    }
+                }
+            }
+            st.pop();
+        }
+        for(auto p:a){
+            st.push(p);
+        }
+    }
+    bool ans=false;
+    while(!st.empty()){
+        if(st.front()==0)ans=true;
+        st.pop();
+    }
+    if(ans)cout<<"String is accepted"<<endl;
+    else cout<<"String is rejected"<<endl;
+    
+}
